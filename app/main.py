fefa1210c9764f78f4c0bf49c0a5c0ef6106b5e1
santhoshchanda <96845855+santhoshchanda@users.noreply.github.com>
2022-01-19 15:24:48 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__) # Initialise the flask app as app
 
 @app.route('/') # Declare the '/' route for app using a route decorator
 def hello_world():
-    return f"You did it. Hello World from {socket.gethostname()}"
+    return f"MK: You did it. Hello World from {socket.gethostname()}"
 
 if __name__ == "__main__":
         app.run(debug=True, host='0.0.0.0')
